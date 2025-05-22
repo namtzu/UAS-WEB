cded5d98cbd263636e7f35d1c6d7a23b42323673
namtzu <usmanmulyadi1001@gmail.com>
2025-05-22 21:08:44 +0700

    nambah gambar

--- UAS_web/app.js
+++ UAS_web/app.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // Header scroll effect for all pages
     window.addEventListener("scroll", function() {
         const header = document.querySelector(".header");
-        if (window.scrollY > 50) {
+        if (window.scrollY > 55) {
             header.classList.add("scrolled");
         } else {
             header.classList.remove("scrolled");
