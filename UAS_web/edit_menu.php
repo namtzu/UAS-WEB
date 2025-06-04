81cdc347448f329872bba480ce300128a7d35487
DarknessLightning <saifani.tri.ramadani.edy.tik23@stu.pnj.ac.id>
2025-06-04 09:33:33 +0700

    add desain

--- UAS_web/edit_menu.php
+++ UAS_web/edit_menu.php
@@ -1,45 +1,106 @@
 
-@ -0,0 +1,106 @@
-<!--input-->
-<form name="form1" method="post" action="">
-    <h2>Input Data Menu</h2>
 
-    <table width="500" border="0">
-        <tr>
-            <td>Kategori: </td>
-            <td><select name="id_kategori">
+<!--input-->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+     <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="shortcut icon" href="favicon.ico" type="image/ico"/>
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    <title>INDEX</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jaro:opsz@6..72&display=swap" rel="stylesheet">
+
+</head>
+<style>
+body{
+    background-color: rgba(253, 246, 239, 1);
+    font-family: 'Poppins', sans-serif;
+  
+}
+.subhead {
+    font-family: "Jaro", sans-serif;
+    font-size:10vh;
+    font-optical-sizing: auto;
+    font-weight: 10;
+    font-style: normal;
+    display: flex;
+    justify-content: center;
+}
+.form {
+    display: flex;
+    justify-content: center;
+}
+.tabel-menu{
+    display: flex;
+    justify-content: center;
+}
+.action {
+  color: #4e261a; /* dark brown */
+  margin-right: 1%
+}
+.data-menu {
+  color: #e87741; /* orange */
+  margin-left: 4px; /* space between words */
+}
+.non-input {
+    text-align: right;
+    vertical-align: top;
+}
+.form input[type = "text"], .form select, .form textarea{
+    width = 320px;
+    box-sizing: border-box;
+}
+
+</style>
+<body>
+   <form name="form1" method="post" action="">
+    <h2 class="subhead"><span class = "action">Input</span><span class = "data-menu">Data Menu</span></h2>
+    <div class="form">
+        <table width="500" border="0" >
+            <tr>
+                <td class = "non-input">Kategori :</td>
+                <td><select name="id_kategori">
                     <option value="">-- Pilih Kategori --</option>
-                    <?php
-                    include "koneksi.php";
-                    $query = mysqli_query($id_mysql, "SELECT idKategori, namaKategori FROM tabel_kategori");
-                    while ($data = mysqli_fetch_assoc($query)) {
-                        echo "<option value='" . $data['idKategori'] . "'>" . $data['namaKategori'] . "</option>";
-                    }
-                    ?>
-                </select>
-            </td>
-        </tr>
-        <tr>
-            <td>Nama Menu</td>
-            <td><input type="text" name="nama_menu"></td>
-        </tr>
-        <tr>
-            <td>Harga Menu</td>
-            <td><input type="text" name="harga_menu"></td>
-        </tr>
-        <tr>
-            <td>Deskripsi Menu</td>
-            <td><textarea name="deskripsi_menu" rows="5" cols="40"></textarea></td>
-        </tr>
-        <tr>
-            <td>Direktori Gambar</td>
-            <td><input type="text" name="dir_gambar"></td>
-        </tr>
-        <tr>
-            <td><input type="submit" name="input" value="input"></td>
-        </tr>
-    </table>
-</form>
+                        <?php
+                        include "koneksi.php";
+                        $query = mysqli_query($id_mysql, "SELECT idKategori, namaKategori FROM tabel_kategori");
+                        while ($data = mysqli_fetch_assoc($query)) {
+                            echo "<option value='" . $data['idKategori'] . "'>" . $data['namaKategori'] . "</option>";
+                        }
+                        ?>
+                        </select>
+                </td>
+            </tr>
+            <tr>
+                <td class = "non-input">Nama Menu : </td>
+                <td><input type="text" name="nama_menu"></td>
+            </tr>
+            <tr>
+                <td class = "non-input">Harga Menu : </td>
+                <td><input type="text" name="harga_menu" ></td>
+            </tr>
+            <tr>
+                <td class = "non-input">Deskripsi Menu : </td>
+                <td><textarea name="deskripsi_menu" rows="5" cols="22"></textarea></td>
+            </tr>
+            <tr>
+                <td class = "non-input">Direktori Gambar : </td>
+                <td><input type="text" name="dir_gambar"></td>
+            </tr>
+            <tr>
+                <td colspan='2' style = "text-align: center;"><input type="submit" name="input" value="input"></td>
+            </tr>
+        </table>
+    </div>
+    </form>
+</body>
 
 <!input data ke database>
 <?php 
@@ -68,32 +129,54 @@ LEFT JOIN tabel_kategori k ON m.id_Kategori = k.idKategori
 
 ");
 
+$menuByKategori = [];
 
-$jumlah_record = mysqli_num_rows($result);
-echo "<h3>Tabel Data Buku</h3>";
-echo "<Table border='1'>";
-echo "<tr>
-<th>ID Menu</th>
-<th>Nama Menu</th>
-<th>Harga Menu</th>
-<th>Deskripsi Menu</th>
-<th>Direktori Gambar</th>
-<th>Kategori</th>
-<th>Proeses</th>
-</tr>";
-while($row = mysqli_fetch_row($result)) {
-    echo "<tr>
-    <td>$row[0]</td>
-    <td>$row[1]</td>
-    <td>$row[2]</td>
-    <td>$row[3]</td>
-    <td>$row[4]</td>
-    <td>$row[5]</td>
-    <td><a href=update_menu.php?id=$row[0]>Update</a> <a href='edit_menu.php?delete_id=$row[0]'>Delete</a></td>
-    </tr>";
+while ($row = mysqli_fetch_assoc($result)) {
+    $kategori = $row['namaKategori'] ?? 'Tanpa Kategori';
+    $menuByKategori[$kategori][] = $row;
 }
-echo "<tr><td colspan='7'>Jumlah Record : $jumlah_record</td></tr>";
-echo "</Table>";
+
+// Tampilkan HTML
+echo "<h3 class='subhead'><span class='action'>Tabel</span><span class='data-menu'>Data Menu</span></h3>";
+echo "<div class='tabel-menu'>";
+echo "<table border='0' style='overflow-y: auto; text-align: center;'>";
+
+foreach ($menuByKategori as $kategori => $items) {
+    // Header kategori
+    echo "<tr class='category-row'>
+            <td colspan='6' style='text-align: center; background-color:rgb(238, 94, 27); color:rgb(78, 38, 26);'><strong>$kategori</strong></td>
+          </tr>";
+
+    // Header kolom menu
+    echo "<tr>
+            <th>ID Menu</th>
+            <th>Nama Menu</th>
+            <th>Harga Menu</th>
+            <th>Deskripsi Menu</th>
+            <th>Direktori Gambar</th>
+            <th>Proses</th>
+          </tr>";
+
+    foreach ($items as $item) {
+        echo "<tr>
+                <td>{$item['idMenu']}</td>
+                <td>{$item['namaMenu']}</td>
+                <td>Rp{$item['hargaMenu']}</td>
+                <td>{$item['deskripsiMenu']}</td>
+                <td>{$item['direktoriGambar']}</td>
+                <td>
+                    <a href='update_menu.php?id={$item['idMenu']}'>Update</a> 
+                    <a href='edit_menu.php?delete_id={$item['idMenu']}'>Delete</a>
+                </td>
+              </tr>";
+    }
+}
+
+echo "</table>";
+echo "</div>";
+
+
+
 ?>
 
 <?php
--- UAS_web/update_menu.php
+++ UAS_web/update_menu.php
@@ -6,29 +6,65 @@ $query = "SELECT*FROM tabel_menu WHERE idMenu='$id_menu'";
 $data = mysqli_query($id_mysql,$query) or die("gagal".mysqli_error());
 $row = mysqli_fetch_array($data);
 ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+     <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="shortcut icon" href="favicon.ico" type="image/ico"/>
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    <title>INDEX</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jaro:opsz@6..72&display=swap" rel="stylesheet">
 
+</head>
+<style>
+body{
+    background-color: rgba(253, 246, 239, 1);
+    font-family: 'Poppins', sans-serif;
+  
+}
+.subhead {
+    font-family: "Jaro", sans-serif;
+    font-size:10vh;
+    font-optical-sizing: auto;
+    font-weight: 10;
+    font-style: normal;
+    display: flex;
+    justify-content: center;
+}
+.form {
+    display: flex;
+    justify-content: center;
+}
+.tabel-menu{
+    display: flex;
+    justify-content: center;
+}
+.action {
+  color: #4e261a; /* dark brown */
+  margin-right: 1%
+}
+.data-menu {
+  color: #e87741; /* orange */
+  margin-left: 4px; /* space between words */
+}
+.non-input {
+    text-align: right;
+    vertical-align: top;
+}
+</style>
 <form name="form1" method="post" action="">
-    <h2>Update Data Menu</h2>
-
+    <h2 class = "subhead"><span class = "action">Update</span><span class = "data-menu">Data Menu</span></h2>
+    <div class = "form">
     <table width="500" border="0">
         <tr>
-            <td>Nama Menu</td>
-            <td><input type="text" name="nama_menu" value="<?php echo"$row[1]";?>"></td>
-        </tr>
-        <tr>
-            <td>Harga Menu</td>
-            <td><input type="text" name="harga_menu" value="<?php echo"$row[2]";?>"></td>
-        </tr>
-        <tr>
-            <td>Deskripsi</td>
-            <td><textarea name="deskripsi" rows="5" cols="40" value=""><?php echo"$row[3]";?></textarea></td>
-        </tr>
-        <tr>
-            <td>Direktori Gambar</td>
-            <td><input type="text" name="direktori" value="<?php echo"$row[4]";?>"></td>
-        </tr>
-        <tr>
-            <td>Kategori</td>
+            <td class = "non-input">Kategori : </td>
             <td><select name="id_kategori">
                     <option value="">-- Pilih Kategori --</option>
                     <?php
@@ -42,9 +78,26 @@ $row = mysqli_fetch_array($data);
             </td>
         </tr>
         <tr>
-            <td><input type="submit" name="update" value="update"></td>
+            <td class = "non-input">Nama Menu : </td>
+            <td><input type="text" name="nama_menu" value="<?php echo"$row[1]";?>"></td>
+        </tr>
+        <tr>
+            <td class = "non-input">Harga Menu : </td>
+            <td><input type="text" name="harga_menu" value="<?php echo"$row[2]";?>"></td>
+        </tr>
+        <tr>
+            <td class = "non-input">Deskripsi Menu : </td>
+            <td><textarea name="deskripsi" rows="5" cols="22" value=""><?php echo"$row[3]";?></textarea></td>
+        </tr>
+        <tr>
+            <td class = "non-input">Direktori Gambar : </td>
+            <td><input type="text" name="direktori" value="<?php echo"$row[4]";?>"></td>
+        </tr>
+        <tr>
+            <td colspan='2' style = "text-align: center;"><input type="submit" name="update" value="update"></td>
         </tr>
     </table>
+    </div>
 </form>
 
 <!update data ke db>
